18c4104ad0064fc5db7d5bac9bee642c6d8bc28e
rstein <r.steinhagen@gsi.de>
2020-09-20 20:49:01 +0200

    updated microservice dependencies and added ZeroMQ proof-of-concepts tests

--- microservice/src/test/java/de/gsi/microservice/concepts/RoundTripAndNotifyTests.java
+++ microservice/src/test/java/de/gsi/microservice/concepts/RoundTripAndNotifyTests.java
@@ -9,23 +9,20 @@ import org.zeromq.ZMQ.Socket;
  *
  * Example output:
  * Setting up test
- * Synchronous round-trip test
- *  3206 calls/second
- * Asynchronous round-trip test
- *  21834 calls/second
- * subscription (SUB) test
- *  689655 calls/second
- * subscription (DEALER) test
- *  30358 calls/second
- * subscription (direct DEALER) test
- *  862068 calls/second
+ * Synchronous round-trip test (TCP)       :        3838 calls/second
+ * Synchronous round-trip test (InProc)    :       12224 calls/second
+ * Asynchronous round-trip test (TCP)      :       33444 calls/second
+ * Asynchronous round-trip test (InProc)   :       35587 calls/second
+ * Subscription (SUB) test                 :      632911 calls/second
+ * Subscription (DEALER) test (TCP)        :       43821 calls/second
+ * Subscription (DEALER) test (InProc)     :       49900 calls/second
+ * Subscription (direct DEALER) test       :     1351351 calls/second
+ * finished tests
  *
  * N.B. for >200000 calls/second the code seems to depend largely on the broker/parameters
  * (ie. JIT, whether services are identified by single characters etc.)
  */
-public class RoundTripAndNotifyTests {
-    private static int SAMPLE_SIZE = 10_000;
-    private static int SAMPLE_SIZE_PUB = 100_000;
+class RoundTripAndNotifyTests {
     // private static final String SUB_TOPIC = "x";
     private static final String SUB_TOPIC = "<domain>/<property>?<filter>#<ctx> - a very long topic to test the dependence of pub/sub pairs on topic lengths";
     private static final byte[] SUB_DATA = "D".getBytes(ZMQ.CHARSET); // custom minimal data
@@ -33,42 +30,139 @@ public class RoundTripAndNotifyTests {
     private static final byte[] WORKER_ID = "W".getBytes(ZMQ.CHARSET); // worker-service name
     private static final byte[] PUBLISH_ID = "P".getBytes(ZMQ.CHARSET); // publish-service name
     private static final byte[] SUBSCRIBER_ID = "S".getBytes(ZMQ.CHARSET); // subscriber name
+    private static int SAMPLE_SIZE = 10_000;
+    private static int SAMPLE_SIZE_PUB = 100_000;
+
+    public static void main(String[] args) {
+        if (args.length == 1) {
+            SAMPLE_SIZE = Integer.parseInt(args[0]);
+            SAMPLE_SIZE_PUB = 10 * SAMPLE_SIZE;
+        }
+
+        Thread brokerThread = new Thread(new Broker());
+        Thread workerThread = new Thread(new Worker());
+        Thread publishThread = new Thread(new PublishWorker());
+        Thread pubDealerThread = new Thread(new PublishDealerWorker());
+        Thread directDealerThread = new Thread(new PublishDirectDealerWorker());
+
+        brokerThread.start();
+        workerThread.start();
+        publishThread.start();
+        pubDealerThread.start();
+        directDealerThread.start();
+
+        Thread clientThread = new Thread(new Client());
+        clientThread.start();
+
+        try {
+            clientThread.join();
+            workerThread.interrupt();
+            brokerThread.interrupt();
+            publishThread.interrupt();
+            pubDealerThread.interrupt();
+            directDealerThread.interrupt();
+
+            // wait for threads to finish
+            workerThread.join();
+            publishThread.join();
+            pubDealerThread.join();
+            directDealerThread.join();
+            brokerThread.join();
+        } catch (InterruptedException e) {
+            // finishes tests
+        }
+        System.out.println("finished tests");
+    }
+
+    private static void measure(final String topic, final int nExec, final Runnable... runnable) {
+        final long start = System.currentTimeMillis();
+
+        for (Runnable run : runnable) {
+            for (int i = 0; i < nExec; i++) {
+                run.run();
+            }
+        }
+
+        final long stop = System.currentTimeMillis();
+        System.out.printf("%-40s:  %10d calls/second\n", topic, (1000 * nExec) / (stop - start));
+    }
 
     static class Broker implements Runnable {
+        private static final int TIMEOUT = 1000;
         @Override
         public void run() {
             try (ZContext ctx = new ZContext()) {
-                Socket frontend = ctx.createSocket(SocketType.ROUTER);
-                Socket backend = ctx.createSocket(SocketType.ROUTER);
-                frontend.setHWM(0);
-                backend.setHWM(0);
-                frontend.bind("tcp://*:5555");
-                backend.bind("tcp://*:5556");
+                Socket tcpFrontend = ctx.createSocket(SocketType.ROUTER);
+                Socket tcpBackend = ctx.createSocket(SocketType.ROUTER);
+                Socket inprocBackend = ctx.createSocket(SocketType.ROUTER);
+                tcpFrontend.setHWM(0);
+                tcpBackend.setHWM(0);
+                inprocBackend.setHWM(0);
+                tcpFrontend.bind("tcp://*:5555");
+                tcpBackend.bind("tcp://*:5556");
+                inprocBackend.bind("inproc://broker");
+
+                Thread internalWorkerThread = new Thread(new InternalWorker(ctx));
+                internalWorkerThread.setDaemon(true);
+                internalWorkerThread.start();
+                Thread internalPublishDealerWorkerThread = new Thread(new InternalPublishDealerWorker(ctx));
+                internalPublishDealerWorkerThread.setDaemon(true);
+                internalPublishDealerWorkerThread.start();
 
                 while (!Thread.currentThread().isInterrupted()) {
-                    ZMQ.Poller items = ctx.createPoller(2);
-                    items.register(frontend, ZMQ.Poller.POLLIN);
-                    items.register(backend, ZMQ.Poller.POLLIN);
+                    // create poller
+                    ZMQ.Poller items = ctx.createPoller(3);
+                    items.register(tcpFrontend, ZMQ.Poller.POLLIN);
+                    items.register(tcpBackend, ZMQ.Poller.POLLIN);
+                    items.register(inprocBackend, ZMQ.Poller.POLLIN);
 
-                    if (items.poll() == -1)
+                    if (items.poll(TIMEOUT) == -1)
                         break; // Interrupted
 
                     if (items.pollin(0)) {
-                        ZMsg msg = ZMsg.recvMsg(frontend);
+                        ZMsg msg = ZMsg.recvMsg(tcpFrontend);
                         if (msg == null)
                             break; // Interrupted
                         ZFrame address = msg.pop();
+                        ZFrame internal = msg.pop();
                         if (address.getData()[0] == CLIENT_ID[0]) {
-                            msg.addFirst(new ZFrame(WORKER_ID));
+                            if ('E' == internal.getData()[0]) {
+                                msg.addFirst(new ZFrame(WORKER_ID));
+                                msg.send(tcpBackend);
+                            } else if ('I' == internal.getData()[0]) {
+                                msg.addFirst(new ZFrame(WORKER_ID));
+                                msg.send(inprocBackend);
+                            }
                         } else {
-                            msg.addFirst(new ZFrame(PUBLISH_ID));
+                            if ('E' == internal.getData()[0]) {
+                                msg.addFirst(new ZFrame(PUBLISH_ID));
+                                msg.send(tcpBackend);
+                            } else if ('I' == internal.getData()[0]) {
+                                msg.addFirst(new ZFrame(PUBLISH_ID));
+                                msg.send(inprocBackend);
+                            }
                         }
                         address.destroy();
-                        msg.send(backend);
                     }
 
                     if (items.pollin(1)) {
-                        ZMsg msg = ZMsg.recvMsg(backend);
+                        ZMsg msg = ZMsg.recvMsg(tcpBackend);
+                        if (msg == null) {
+                            break; // Interrupted
+                        }
+                        ZFrame address = msg.pop();
+
+                        if (address.getData()[0] == WORKER_ID[0]) {
+                            msg.addFirst(new ZFrame(CLIENT_ID));
+                        } else {
+                            msg.addFirst(new ZFrame(SUBSCRIBER_ID));
+                        }
+                        msg.send(tcpFrontend);
+                        address.destroy();
+                    }
+
+                    if (items.pollin(2)) {
+                        ZMsg msg = ZMsg.recvMsg(inprocBackend);
                         if (msg == null)
                             break; // Interrupted
                         ZFrame address = msg.pop();
@@ -78,11 +172,74 @@ public class RoundTripAndNotifyTests {
                         } else {
                             msg.addFirst(new ZFrame(SUBSCRIBER_ID));
                         }
-                        msg.send(frontend);
                         address.destroy();
+                        msg.send(tcpFrontend);
                     }
 
                     items.close();
+                }
+
+                internalWorkerThread.interrupt();
+                internalPublishDealerWorkerThread.interrupt();
+                if (!internalWorkerThread.isInterrupted()) {
+                    internalWorkerThread.join();
+                }
+                if (!internalPublishDealerWorkerThread.isInterrupted()) {
+                    internalPublishDealerWorkerThread.join();
+                }
+            } catch (InterruptedException | IllegalStateException e) {
+                // terminated broker via interrupt
+            }
+        }
+
+        static class InternalWorker implements Runnable {
+            private final ZContext ctx;
+            public InternalWorker(ZContext ctx) {
+                this.ctx = ctx;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    Socket worker = ctx.createSocket(SocketType.DEALER);
+                    worker.setHWM(0);
+                    worker.setIdentity(WORKER_ID);
+                    worker.connect("inproc://broker");
+                    while (!Thread.currentThread().isInterrupted()) {
+                        ZMsg msg = ZMsg.recvMsg(worker);
+                        msg.send(worker);
+                    }
+                } catch (ZMQException e) {
+                    // terminate internal worker
+                }
+            }
+        }
+
+        static class InternalPublishDealerWorker implements Runnable {
+            private final ZContext ctx;
+            public InternalPublishDealerWorker(ZContext ctx) {
+                this.ctx = ctx;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    Socket worker = ctx.createSocket(SocketType.DEALER);
+                    worker.setHWM(0);
+                    worker.setIdentity(PUBLISH_ID);
+                    worker.connect("inproc://broker");
+                    while (!Thread.currentThread().isInterrupted()) {
+                        ZMsg msg = ZMsg.recvMsg(worker);
+                        if ("start".equals(msg.getFirst().getString(ZMQ.CHARSET))) {
+                            // System.err.println("dealer (indirect): start pushing");
+                            for (int requests = 0; requests < SAMPLE_SIZE_PUB; requests++) {
+                                worker.send(SUB_TOPIC, ZMQ.SNDMORE);
+                                worker.send(SUB_DATA);
+                            }
+                        }
+                    }
+                } catch (ZMQException | IllegalStateException e) {
+                    // terminate internal publish worker
                 }
             }
         }
@@ -185,48 +342,46 @@ public class RoundTripAndNotifyTests {
                 Socket subClient = ctx.createSocket(SocketType.SUB);
                 subClient.setHWM(0);
                 subClient.connect("tcp://localhost:5557");
+
                 System.out.println("Setting up test");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+                // check and wait until broker is up and running
+                ZMsg.newStringMsg("E").addString("hello").send(client);
+                ZMsg.recvMsg(client).destroy();
 
-                long start;
-
-                System.out.println("Synchronous round-trip test");
-                start = System.currentTimeMillis();
-
-                for (int requests = 0; requests < SAMPLE_SIZE; requests++) {
+                measure("Synchronous round-trip test (TCP)", SAMPLE_SIZE, () -> {
                     ZMsg req = new ZMsg();
+                    req.addString("E");
                     req.addString("hello");
                     req.send(client);
                     ZMsg.recvMsg(client).destroy();
-                }
+                });
 
-                long now = System.currentTimeMillis();
-                System.out.printf(
-                        " %d calls/second\n", (1000 * SAMPLE_SIZE) / (now - start));
-
-                System.out.println("Asynchronous round-trip test");
-                start = System.currentTimeMillis();
-
-                for (int requests = 0; requests < SAMPLE_SIZE; requests++) {
+                measure("Synchronous round-trip test (InProc)", SAMPLE_SIZE, () -> {
                     ZMsg req = new ZMsg();
+                    req.addString("I");
                     req.addString("hello");
                     req.send(client);
-                }
-
-                for (int requests = 0;
-                        requests < SAMPLE_SIZE && !Thread.currentThread().isInterrupted();
-                        requests++) {
                     ZMsg.recvMsg(client).destroy();
-                }
+                });
 
-                long now2 = System.currentTimeMillis();
-                System.out.printf(" %d calls/second\n", (1000 * SAMPLE_SIZE) / (now2 - start));
+                measure("Asynchronous round-trip test (TCP)", SAMPLE_SIZE, () -> {
+                    // send messages
+                    ZMsg req = new ZMsg();
+                    req.addString("E");
+                    req.addString("hello");
+                    req.send(client); }, () -> {
+                    // receive messages
+                    ZMsg.recvMsg(client).destroy(); });
 
-                System.out.println("subscription (SUB) test");
+                measure("Asynchronous round-trip test (InProc)", SAMPLE_SIZE, () -> {
+                    // send messages
+                    ZMsg req = new ZMsg();
+                    req.addString("I");
+                    req.addString("hello");
+                    req.send(client); }, () -> {
+                    // receive messages
+                    ZMsg.recvMsg(client).destroy(); });
+
                 subClient.subscribe(SUB_TOPIC.getBytes(ZMQ.CHARSET));
                 // first loop to empty potential queues/HWM
                 for (int requests = 0; requests < SAMPLE_SIZE_PUB; requests++) {
@@ -234,84 +389,38 @@ public class RoundTripAndNotifyTests {
                     req.destroy();
                 }
                 // start actual subscription loop
-                start = System.currentTimeMillis();
-                for (int requests = 0; requests < SAMPLE_SIZE_PUB; requests++) {
+                measure("Subscription (SUB) test", SAMPLE_SIZE_PUB, () -> {
                     ZMsg req = ZMsg.recvMsg(subClient);
                     req.destroy();
-                }
-                long now3 = System.currentTimeMillis();
+                });
                 subClient.unsubscribe(SUB_TOPIC.getBytes(ZMQ.CHARSET));
-                System.out.printf(" %d calls/second\n", (1000 * SAMPLE_SIZE_PUB) / (now3 - start));
 
-                System.out.println("subscription (DEALER) test");
                 client.disconnect("tcp://localhost:5555");
                 client.setIdentity(SUBSCRIBER_ID);
                 client.connect("tcp://localhost:5555");
-                ZMsg.newStringMsg("start").send(client);
-                start = System.currentTimeMillis();
-                for (int requests = 0; requests < SAMPLE_SIZE_PUB; requests++) {
+                ZMsg.newStringMsg("start").addFirst("E").send(client);
+                measure("Subscription (DEALER) test (TCP)", SAMPLE_SIZE_PUB, () -> {
                     ZMsg req = ZMsg.recvMsg(client);
                     req.destroy();
-                }
-                long now4 = System.currentTimeMillis();
-                System.out.printf(" %d calls/second\n", (1000 * SAMPLE_SIZE_PUB) / (now4 - start));
+                });
 
-                System.out.println("subscription (direct DEALER) test");
+                ZMsg.newStringMsg("start").addFirst("I").send(client);
+                measure("Subscription (DEALER) test (InProc)", SAMPLE_SIZE_PUB, () -> {
+                    ZMsg req = ZMsg.recvMsg(client);
+                    req.destroy();
+                });
+
                 client.disconnect("tcp://localhost:5555");
                 client.connect("tcp://localhost:5558");
                 ZMsg.newStringMsg("start").send(client);
-                start = System.currentTimeMillis();
-                for (int requests = 0; requests < SAMPLE_SIZE_PUB; requests++) {
+                measure("Subscription (direct DEALER) test", SAMPLE_SIZE_PUB, () -> {
                     ZMsg req = ZMsg.recvMsg(client);
                     req.destroy();
-                }
-                long now5 = System.currentTimeMillis();
-                System.out.printf(" %d calls/second\n", (1000 * SAMPLE_SIZE_PUB) / (now5 - start));
+                });
 
             } catch (ZMQException e) {
                 System.out.println("terminate client");
             }
-        }
-    }
-
-    public static void main(String[] args) {
-        if (args.length == 1) {
-            SAMPLE_SIZE = Integer.parseInt(args[0]);
-            SAMPLE_SIZE_PUB = 10 * SAMPLE_SIZE;
-        }
-
-        Thread brokerThread = new Thread(new Broker());
-        Thread workerThread = new Thread(new Worker());
-        Thread publishThread = new Thread(new PublishWorker());
-        Thread pubDealerThread = new Thread(new PublishDealerWorker());
-        Thread directDealerThread = new Thread(new PublishDirectDealerWorker());
-
-        Thread clientThread = new Thread(new Client());
-
-        brokerThread.setDaemon(true);
-        workerThread.setDaemon(true);
-        publishThread.setDaemon(true);
-        pubDealerThread.setDaemon(true);
-        directDealerThread.setDaemon(true);
-
-        brokerThread.start();
-        workerThread.start();
-        publishThread.start();
-        pubDealerThread.start();
-        directDealerThread.start();
-
-        clientThread.start();
-
-        try {
-            clientThread.join();
-            workerThread.interrupt();
-            brokerThread.interrupt();
-            publishThread.interrupt();
-            pubDealerThread.interrupt();
-            directDealerThread.interrupt();
-            Thread.sleep(200); // give them some time
-        } catch (InterruptedException e) {
-            // finishes tests
         }
     }
 }
